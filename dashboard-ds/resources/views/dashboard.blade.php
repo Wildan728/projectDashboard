84b2c7d130a834c572b29603a3b2e35ff2fe2191
MGilangSeftian <gilangseftian90@gmail.com>
2025-04-26 10:33:05 +0700

    update

--- dashboard-ds/resources/views/dashboard.blade.php
+++ dashboard-ds/resources/views/dashboard.blade.php
@@ -3,6 +3,6 @@
 @section('content')
     <div class="mb-8">
         <h1 class="text-4xl font-extrabold text-gray-800 tracking-tight">Dashboard</h1>
-        <p class="text-gray-500 mt-1">Selamat datang kembali, Admin!</p>
+        <p class="text-gray-500 mt-1">Selamat datang kembali, admin!</p>
     </div>
 @endsection
